965285fc9c7e7253511b48ed568c0c8e8eb62be6
nyan_sama <nyan_sama@qq.com>
2025-07-10 12:53:33 +0800

    feat: changed log file postion to assets/getto/log/ and added log rotation

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,15 +1,16 @@
 import logging
 import pathlib
+from logging.handlers import TimedRotatingFileHandler
 
 # Create log directory
-pathlib.Path("./assets/cache/debug").mkdir(parents=True, exist_ok=True)
+pathlib.Path("./assets/getto/log").mkdir(parents=True, exist_ok=True)
 
 # Configure logger
 logging.basicConfig(
     format="[%(asctime)s][%(levelname)s][%(filename)s][L%(lineno)d][%(funcName)s] | %(message)s",
     level=logging.DEBUG,
     handlers=[
-        logging.FileHandler("./assets/cache/debug/mlw-getto.log", encoding="utf-8"),
+        TimedRotatingFileHandler("./assets/getto/log/getto.log", when="midnight", interval=1, backupCount=7, encoding="utf-8"),
         logging.StreamHandler(),
     ],
 )
